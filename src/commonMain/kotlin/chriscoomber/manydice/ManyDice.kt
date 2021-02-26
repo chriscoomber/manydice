62c8c7419c0885d694b3191b6da6b16cc509b12d
chriscoomber <chrisper00@gmail.com>
2021-02-26 00:45:06 +0000

    Sort out kotlin multiplatform project barebones

---
+++ src/commonMain/kotlin/chriscoomber/manydice/ManyDice.kt
@@ -0,0 +1,5 @@
+package chriscoomber.manydice
+
+object ManyDice {
+    const val TEST_TEXT = "Hello from ManyDice"
+}
---
+++ src/commonTest/kotlin/chriscoomber/manydice/ManyDiceTest.kt
@@ -0,0 +1,11 @@
+package chriscoomber.manydice
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ManyDiceTest {
+    @Test
+    fun testHello() {
+        assertEquals(ManyDice.TEST_TEXT, "Hello from ManyDice")
+    }
+}
