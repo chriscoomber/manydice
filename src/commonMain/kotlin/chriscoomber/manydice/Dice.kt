80909a130ce2375f2e9324a5637a938cde4c92cc
chriscoomber <chrisper00@gmail.com>
2021-03-01 17:54:07 +0000

    Add default dice names

--- src/commonMain/kotlin/chriscoomber/manydice/Dice.kt
+++ src/commonMain/kotlin/chriscoomber/manydice/Dice.kt
@@ -3,7 +3,7 @@ package chriscoomber.manydice
 /**
  * Construct a new fair dice from its number of faces, numbered from 1 to [faces].
  */
-fun fairDice(faces: Int, name: String? = null): FiniteRandomVariable<Int> =
+fun fairDice(faces: Int, name: String = "1d$faces"): FiniteRandomVariable<Int> =
     PhysicalRandomVariable.fromProbabilityMassFunction(
         (1..faces).associateWith { 1f/faces },
         name
@@ -13,19 +13,19 @@ fun fairDice(faces: Int, name: String? = null): FiniteRandomVariable<Int> =
  * Construct a new fair dice with faces labelled differently. Pass in a map from face values to the
  * number of faces with that value.
  */
-fun <T> fairDice(facesMap: Map<T, Int>, name: String? = null): FiniteRandomVariable<T> =
+fun <T> fairDice(facesMap: Map<T, Int>, name: String = "custom d${facesMap.values.sum()}"): FiniteRandomVariable<T> =
     PhysicalRandomVariable.fromProbabilityMassFunction(
         facesMap.mapValues { (_, numFaces) -> numFaces.toFloat() / facesMap.values.sum() },
         name
     )
 
-fun fairDiceSum(quantity: Int, faces: Int, name: String? = null): FiniteRandomVariable<Int> {
+fun fairDiceSum(quantity: Int, faces: Int, name: String = "${quantity}d$faces"): FiniteRandomVariable<Int> {
     var acc = PhysicalRandomVariable.fromProbabilityMassFunction(mapOf(0 to 1f)) as FiniteRandomVariable<Int>  // Start with a dice that's always 0
 
     repeat(quantity) {
         acc = (acc + fairDice(faces)).forgetDependencies()
     }
 
-    return if (name != null) acc.setName(name) else acc
+    return acc.setName(name)
 }
 
